3a7428fa4eb6156f0d33c4206d1615a3e95f6016
AIWiz2 <aiwiz2052@gmail.com>
2026-02-28 11:26:03 +0000

    Update index.js

--- gift/index.js
+++ gift/index.js
@@ -1,28 +1,42 @@
 const fs = require('fs');
+const crypto = require('crypto');
 
-function giftedId(num = 4) {
-  let result = "";
-  let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  var characters9 = characters.length;
-  for (var i = 0; i < num; i++) {
-    result += characters.charAt(Math.floor(Math.random() * characters9));
+/**
+ * Generate ECOWSCO-MD branded ID
+ * Example: ECOWSCO-MD-A8K29L
+ */
+function ecowscoId(length = 6) {
+  const random = crypto
+    .randomBytes(length)
+    .toString("hex")
+    .slice(0, length)
+    .toUpperCase();
+
+  return "ECOWSCO-MD-" + random;
+}
+
+/**
+ * Generate 8-character random code
+ */
+function generateRandomCode() {
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  let result = '';
+
+  for (let i = 0; i < 8; i++) {
+    result += chars.charAt(Math.floor(Math.random() * chars.length));
   }
+
   return result;
 }
 
-function generateRandomCode() {
-    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-    let result = '';
-    for (let i = 0; i < 8; i++) {
-        result += chars.charAt(Math.floor(Math.random() * chars.length));
-    }
-    return result;
-}
-
+/**
+ * Remove file or folder safely
+ */
 async function removeFile(FilePath) {
-    if (!fs.existsSync(FilePath)) return false;
-    await fs.promises.rm(FilePath, { recursive: true, force: true });
-    return true;
+  if (!fs.existsSync(FilePath)) return false;
+
+  await fs.promises.rm(FilePath, { recursive: true, force: true });
+  return true;
 }
 
-module.exports = { giftedId, removeFile, generateRandomCode };
+module.exports = { ecowscoId, removeFile, generateRandomCode };
